7ed798d08051bf6452194cf77a1ac4031107df76
Eli Boninger <eboninger@gmail.com>
2024-12-05 14:49:16 -0500

    use client-side link tag

--- src/Authorize.jsx
+++ src/Authorize.jsx
@@ -1,11 +1,9 @@
-import axios from 'axios';
-
 const Authorize = () => {
-    const handleAuth = async () => {
-        axios.get(`${import.meta.env.API_URL}/twitter-auth`);
-    };
+    const scopes = ['tweet.read', 'tweet.write'].join('%20');
+    const url = `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=${import.meta.env.VITE_X_CLIENT_ID}&redirect_uri=${encodeURIComponent(import.meta.env.VITE_CALLBACK_URI)}&scope=${scopes}&state=state&code_challenge=challenge&code_challenge_method=plain`;
 
-    return (<button onClick={handleAuth}>Authenticate</button>);
+
+    return (<a href={url}>Authenticate</a>);
 };
 
 export default Authorize;
