e5b975f8e60c8bf6de2e992b2a0763543cc09fb9
Eli Boninger <eboninger@gmail.com>
2024-12-05 15:58:16 -0500

    update app scopes

--- src/Authorize.jsx
+++ src/Authorize.jsx
@@ -1,5 +1,5 @@
 const Authorize = () => {
-    const scopes = ['tweet.read', 'tweet.write'].join('%20');
+    const scopes = ['tweet.read', 'tweet.write', 'users.read'].join('%20');
     const url = `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=${import.meta.env.VITE_X_CLIENT_ID}&redirect_uri=${encodeURIComponent(import.meta.env.VITE_CALLBACK_URI)}&scope=${scopes}&state=state&code_challenge=challenge&code_challenge_method=plain`;
 
 
