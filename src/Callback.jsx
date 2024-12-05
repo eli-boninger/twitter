5545bdf2e5dabebbe3e8251e3f709dbf6f60b5e5
Eli Boninger <eboninger@gmail.com>
2024-12-05 17:29:42 -0500

    add backend tweet call

--- src/Callback.jsx
+++ src/Callback.jsx
@@ -13,7 +13,7 @@ export const Callback = () => {
         if (!token) {
             console.error("Unauthorized");
         } else {
-            const res = await axios.post('https://api.x.com/2/tweets', { text: "Hi!" }, { headers: { Authorization: `Bearer ${token}` } });
+            const res = await axios.post(`${VITE_API_URL}/tweet?token=${token}`, { text: "Hi!" });
             console.log(res.data);
         }
     };
