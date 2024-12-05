a4c37123e4dc8f753131121e231f3f9914921f9e
Eli Boninger <eboninger@gmail.com>
2024-12-05 17:33:18 -0500

    add proper env var prefix

--- src/Callback.jsx
+++ src/Callback.jsx
@@ -13,7 +13,7 @@ export const Callback = () => {
         if (!token) {
             console.error("Unauthorized");
         } else {
-            const res = await axios.post(`${VITE_API_URL}/tweet?token=${token}`, { text: "Hi!" });
+            const res = await axios.post(`${import.meta.env.VITE_API_URL}/tweet?token=${token}`, { text: "Hi!" });
             console.log(res.data);
         }
     };
