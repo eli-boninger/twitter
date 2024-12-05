7a7c46f3fea6b72d719888036b60a8755d0bf957
Eli Boninger <eboninger@gmail.com>
2024-12-05 17:21:30 -0500

    add tweet button

--- src/Callback.jsx
+++ src/Callback.jsx
@@ -4,15 +4,26 @@ import { useSearchParams } from "react-router-dom";
 
 export const Callback = () => {
     const [searchParams, _setSearchParams] = useSearchParams();
+
     const state = searchParams.get('state');
     const code = searchParams.get('code');
+
+    const tweet = async () => {
+        const token = sessionStorage.getItem("access_token");
+        if (!token) {
+            console.error("Unauthorized");
+        } else {
+            const res = await axios.post('https://api.x.com/2/tweets', { text: "Hi!" }, { headers: { Authorization: `Bearer ${token}` } });
+            console.log(res.data);
+        }
+    };
 
     useEffect(() => {
         async function getOAuthToken() {
 
             try {
                 const res = await axios.post(`${import.meta.env.VITE_API_URL}/token`, { state, code });
-                console.log(res.data);
+                sessionStorage.setItem('access_token', res.data.access_token);
             } catch (e) {
                 console.log(e);
             }
@@ -24,6 +35,10 @@ export const Callback = () => {
     }, [state, code]);
 
     return (
-        <div>Callback {JSON.stringify(searchParams)}</div>
+        <>
+            <div>Callback</div>
+            <button onClick={tweet}>Tweet "Hi!"</button>
+        </>
+
     );
 };
