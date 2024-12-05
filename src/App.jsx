0bb66fae769b8c0def1d2096034e048496c512d0
Eli Boninger <eboninger@gmail.com>
2024-12-05 14:26:28 -0500

    componentize

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Callback } from './Callback';
-import axios from 'axios';
+import Authorize from './Authorize';
 
 function App() {
 
-  const handleAuth = async () => {
-    const scopes = ['tweet.read', 'tweet.write'].join('%20');
-    await axios.get(`https://twitter.com/i/oauth2/authorize?response_type=code&client_id=${import.meta.env.VITE_X_CLIENT_ID}&redirect_uri=${encodeURIComponent(import.meta.env.VITE_CALLBACK_URI)}&scope=&state=state&code_challenge=challenge&code_challenge_method=plain`);
-  };
+
 
   return (
     <>
       <BrowserRouter>
-        <button onClick={handleAuth}>Authenticate</button>
         <Routes>
+          <Route path="/" element={<Authorize />} />
           <Route path="/callback" element={<Callback />} />
         </Routes>
       </BrowserRouter>
