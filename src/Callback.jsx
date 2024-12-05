5b019644dd253e7ef48d275f4e3205233557514e
Eli Boninger <eboninger@gmail.com>
2024-12-05 15:07:37 -0500

    Fix typo in post url to x api

--- src/Callback.jsx
+++ src/Callback.jsx
@@ -15,7 +15,7 @@ export const Callback = () => {
             params.append('grant_type', 'authorization_code');
             params.append('redirect_uri', import.meta.env.VITE_CALLBACK_URI);
             params.append('code_verifier', 'challenge');
-            const res = await axios.post('https://api.x.com/2/oauth/token', params, { headers: { "Content-Type": "application/x-www-form-urlencoded" } });
+            const res = await axios.post('https://api.x.com/2/oauth2/token', params, { headers: { "Content-Type": "application/x-www-form-urlencoded" } });
             console.log(res.data);
         }
         if (state && code) {
