60f9c815cb53fe64ea8af7a2339302edce0be50b
Eli Boninger <eboninger@gmail.com>
2024-12-05 14:31:33 -0500

    Use api for auth

--- src/Authorize.jsx
+++ src/Authorize.jsx
@@ -2,8 +2,7 @@ import axios from 'axios';
 
 const Authorize = () => {
     const handleAuth = async () => {
-        const scopes = ['tweet.read', 'tweet.write'].join('%20');
-        await axios.get(`https://twitter.com/i/oauth2/authorize?response_type=code&client_id=${import.meta.env.VITE_X_CLIENT_ID}&redirect_uri=${encodeURIComponent(import.meta.env.VITE_CALLBACK_URI)}&scope=${scopes}&state=state&code_challenge=challenge&code_challenge_method=plain`);
+        axios.get(`${import.meta.env.API_URL}/twitter-auth`);
     };
 
     return (<button onClick={handleAuth}>Authenticate</button>);
